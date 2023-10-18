7c2b2aa6be7f46b316d00828476d4c31209bcd4a
Vuyisanani <vuyisanani@ymail.com>
2023-10-18 11:15:28 -0700

    Updated Help built-in commands

---
+++ help_builtin1.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+/**
+ * help_env - will show information on shell builtin command 'env'.
+ */
+
+void help_env(void)
+
+{
+	char *msg = "env: env\n\tPrints the current environment.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * help_setenv - will show information on shell builtin command 'setenv'.
+ */
+
+void help_setenv(void)
+
+{
+	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environment variable, or modifies an existing one.\n\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\tUpon failure, prints a message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * help_unsetenv - will show information on shell builtin command
+ * 'unsetenv'.
+ */
+
+void help_unsetenv(void)
+
+{
+	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environmental variable.\n\n\tUpon failure, prints a ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
